366d0156f6ed0cb4bc314a4d651da0c4bf30a8df
Jklein64 <jklein.64@verizon.net>
2020-12-07 17:21:28 -0500

    Update params

--- src/index.ts
+++ src/index.ts
@@ -38,28 +38,30 @@ class QueryError extends Error {}
 
 function isValidRequest(query: typeof express.request.query) {
 	if (typeof query.quantity !== "string")
-		throw new QueryError("Query did not provide a valid quantity parameter. Quantity must be of type `string`.")
+		throw new QueryError("Query did not provide a valid `quantity` parameter. Quantity must be of type `string`.")
 	if (!/^[0-9]+$/.test(query.quantity))
 		throw new QueryError(
-			"Query did not provide a valid quantity parameter. Quantity must be of type `string` with only characters 0 through 9."
+			"Query did not provide a valid `quantity` parameter. Quantity must be of type `string` with only characters 0 through 9."
 		)
 
 	if (typeof query.size !== "string")
-		throw new QueryError("Query did not provide a valid size parameter. Size must be of type `string`.")
+		throw new QueryError("Query did not provide a valid `size` parameter. Size must be of type `string`.")
 	if (!/^[0-9]+$/.test(query.size))
 		throw new QueryError(
-			"Query did not provide a valid size parameter. Size must be of type `string` and represent a number."
+			"Query did not provide a valid `size` parameter. Size must be of type `string` and represent a number."
 		)
 	if (parseInt(query.size) > 32)
 		throw new QueryError(
-			"Query did not provide a valid size parameter. Size must be of type `string` and represent a number no larger than 32."
+			"Query did not provide a valid `size` parameter. Size must be of type `string` and represent a number no larger than 32."
 		)
 
 	if (typeof query.difficulty !== "string")
-		throw new QueryError("Query did not provide a valid difficulty parameter. Difficulty must be of type `string`.")
+		throw new QueryError(
+			"Query did not provide a valid `difficulty` parameter. Difficulty must be of type `string`."
+		)
 	if (!["Trivial", "Basic", "Intermediate", "Advanced", "Extreme", "Unreasonable"].includes(query.difficulty))
 		throw new QueryError(
-			"Query did not provide a valid difficulty parameter. Difficulty must be of type `string` and be one of `Trivial`, `Basic`, `Intermediate`, `Advanced`, `Extreme`, or `Unreasonable`."
+			"Query did not provide a valid `difficulty` parameter. Difficulty must be of type `string` and be one of `Trivial`, `Basic`, `Intermediate`, `Advanced`, `Extreme`, or `Unreasonable`."
 		)
 	return true
 }
